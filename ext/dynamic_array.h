d44d8ae61abf2c54d7784bf46073de397d88f7fc
Rory Molinari <rorymolinari@gmail.com>
2023-01-29 11:25:44 -0500

    C Extension: macro names; .o depends on dynamic_array.h

--- ext/dynamic_array.h
+++ ext/dynamic_array.h
@@ -17,27 +17,41 @@
  * - there will be a problem with duplicated code if two files each use DYNAMIC_ARRAY_OF(the_same_type). I'm sure the linker will
  *   complain.
  *   - note that we can't use the preprocessor to avoid defining everything multiple times because we can't nest #defines.
- *   - maybe it would be simpler to use a different preprocessor to generate sourcefiles from a template and do things via the Makefile.
+ *   - maybe it would be simpler to use a different preprocessor to generate sourcefiles from a template and do things via the
+ *     Makefile.
+ *     - ugh :(
  */
 
 /*
  * The name of the type being defined.
  *
- * For example, when defining a dynamic array storing longs, this will be DynamicArray_long
+ * For example, when defining a dynamic array storing longs, this will be DynamicArray_long.
+ *
+ * See below for the need for different handling for, say, __DA_TYPE(unsigned long).
  */
 #define __DA_TYPE(type) DynamicArray_##type
 
 /*
  * Code for a dynamic array storing elements of the given type.
  *
- * Note the interaction between the (long) macro and embedded C comments. The preprocessor elminates comments in phase 3 - replacing
- * each with a single space - and expands macros in phase 4. Thus each multi-line comment below is the same as a space so far as
- * this macro is concerned. Having backslashes in the comments themselves causes some sort of other problem. Thus, although it looks
- * like the macro expansion text ends with the first comment, it doesn't.
+ * Note the interaction between the macro and embedded C comments. The preprocessor elminates comments in phase 3 - replacing each
+ * with a single space - and expands macros in phase 4. Thus each multi-line comment below is the same as a space so far as the
+ * macro is concerned. Having backslashes in the comments themselves causes some sort of other problem. Thus, although it looks like
+ * the macro expansion text ends with the first comment, it doesn't.
  *
  * Reference: https://stackoverflow.com/questions/1510869/does-the-c-preprocessor-strip-comments-or-expand-macros-first
+ *
+ * TODO:
+ * - handle types with spaces, like "unsigned long", or with non-alphanumerics, like "void *".
+ *   - presuambly we need a second argument, suffix, for use in identifiers. Client code can provide, say, "ulong" and "void_ptr"
+ *   - for standard types we could use _Generic along the lines of Alex Grey's answer here:
+ *     https://stackoverflow.com/questions/9804371/syntax-and-sample-usage-of-generic-in-c11
+ *     - Then, if this is DEFINE_DYNAMIC_ARRAY_OF2(type, suffix), we could define a macro TYPE_SUFFIX(type) using _Generic and
+ *       then do
+ *
+ *         #define DEFINE_DYNAMIC_ARRAY_OF(type) DEFINE_DYNAMIC_ARRAY_OF2(type, TYPE_SUFFIX(type))
  */
-#define DYNAMIC_ARRAY_OF(type)                                                                                                      \
+#define DEFINE_DYNAMIC_ARRAY_OF(type)                                                                                               \
 typedef struct {                                                                                                                    \
   type *array;                                                                                                                      \
   size_t size;                                                                                                                      \
@@ -45,8 +59,8 @@ typedef struct {                                                                
 } __DA_TYPE(type);                                                                                                                  \
                                                                                                                                     \
 /*
- * Initialize a DynamicArray struct with the given initial size and with all values set to the default value. The default value is
- * stored and used to initialize new array sections if and when the array needs to be expanded.
+ * Initialize an already-allocated DynamicArray struct with the given initial size and with all elements set to the default
+ * value. The default value is stored and used to initialize new array sections if and when the array needs to be expanded.
  */                                                                                                                                 \
 void initDynamicArray_##type(__DA_TYPE(type) *a, size_t initial_size, type default_val) {                                           \
   a->array = malloc(initial_size * sizeof(type));                                                                                   \
@@ -60,13 +74,18 @@ void initDynamicArray_##type(__DA_TYPE(type) *a, size_t initial_size, type defau
                                                                                                                                     \
 /*
  * Assign +value+ to the the +index+-th element of the array, increasing the size of the array if necessary.
+ *
+ * If expansion is required, each new element is first initialized to the default value.
  */                                                                                                                                 \
 void assignInDynamicArray_##type(__DA_TYPE(type) *a, size_t index, type value) {                                                    \
   if (a->size <= index) {                                                                                                           \
     size_t new_size = a->size;                                                                                                      \
     while (new_size <= index) {                                                                                                     \
-      new_size = 8 * new_size / 5 + 8; /* 8/5 gives "Fibonnacci-like" growth; adding 8 to avoid small arrays having to reallocate
-                                        * too often as they grow. Who knows if it's worth being "clever".*/                         \
+      /*
+       * 8/5 gives "Fibonnacci-like" growth; adding 8 avoids small arrays having to reallocate too often as they grow. Who knows if
+       * it's worth being "clever".
+       */                                                                                                                           \
+      new_size = 8 * new_size / 5 + 8;                                                                                              \
     }                                                                                                                               \
                                                                                                                                     \
     type *new_array = realloc(a->array, new_size * sizeof(type));                                                                   \
@@ -95,10 +114,10 @@ void freeDynamicArray_##type(__DA_TYPE(type) *a) {                              
 }                                                                                                                                   \
                                                                                                                                     \
 /*
- * Return the amount of heap space allocated for the object. This might be useful to the Ruby runtime.
+ * Return the amount of heap space allocated for the object, including the object itself. This might be useful to the Ruby runtime.
  */                                                                                                                                 \
 size_t _size_of_##type(__DA_TYPE(type) *a) {                                                                                        \
-  return sizeof(__DA_TYPE(type)) + a->size * sizeof(a->default_val);                                                                \
+  return sizeof(a) + a->size * sizeof(type);                                                                                        \
 }
 
 /*
@@ -106,7 +125,9 @@ size_t _size_of_##type(__DA_TYPE(type) *a) {                                    
  *
  * For example, when a DynamicArray_long is defined, we can drop the "_long" when calling the functions (though not from the struct
  * name).
-*/
+ *
+ * Client code using a type we don't know about will need to #define these itself.
+ */
 #define initDynamicArray(a, initial_size, default_val)                                                                              \
   _Generic((a),                                                                                                                     \
            __DA_TYPE(long)* : initDynamicArray_long)((a), (initial_size), (default_val))
